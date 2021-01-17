1ee11c8115cb141035a9e7df4156101648bfab92
Will Usher <will.usher@hey.com>
2021-01-16 23:25:12 -0700

    initial commit, serial version

---
+++ README.md
@@ -0,0 +1,5 @@
+# parallel-rsync
+
+Python script to simplify parallelizing transferring directories
+using rsync.
+
---
+++ parallel-rsync.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+import re
+
+USAGE = """Usage:
+./parallel_rsync <from> <to>
+
+Documentation:
+    <from>, <to>    The directory to transfer from, or the output location to
+                    transfer to. Either can be a local or remote path, though
+                    for rsync at least one must be local. Paths are specified
+                    as they are to rsync
+"""
+
+def path_is_remote(path):
+    return ":" in path
+
+def get_local_file_list(local_path):
+    file_list = []
+    if os.path.isdir(local_path):
+        for path, dirs, files in os.walk(local_path):
+            for f in files:
+                filename = os.path.join(path, f)
+                file_list.append(filename)
+    else:
+        file_list.append(local_path)
+
+    # Change all paths to be relative to local path
+    for i in range(len(file_list)):
+        file_list[i] = os.path.relpath(file_list[i], local_path)
+    return file_list
+
+def get_remote_file_list(rem_path):
+    match_file = re.compile("([d-][rwx-]+)[ ]+[0-9,]+ \d+\/\d+\/\d+ \d+\:\d+\:\d+ (.*)")
+
+    remote_info = rem_path.split(":")
+    remote_paths = [remote_info[1]]
+    file_list = []
+    while len(remote_paths) > 0:
+        path = remote_paths.pop(0)
+        result = subprocess.run(["rsync", "-s", f"{remote_info[0]}:{path}"], capture_output=True)
+        if result.stderr:
+            print(f"Error listing remote path: {result.stderr.decode('utf8')}")
+            continue
+
+        stdout = result.stdout.decode("utf8")
+        for m in match_file.finditer(stdout):
+            is_dir = m.group(1)[0] == "d"
+            f = m.group(2)
+            if f == ".":
+                continue
+
+            if is_dir:
+                if path != f:
+                    remote_paths.append(os.path.join(path, f) + "/")
+                else:
+                    remote_paths.append(f + "/")
+            else:
+                file_list.append(os.path.join(path, f))
+
+    # Change all paths to be relative to the original remote path
+    for i in range(len(file_list)):
+        file_list[i] = os.path.relpath(file_list[i], remote_info[1])
+    return file_list
+
+def get_file_list(path):
+    if path_is_remote(path):
+        return get_remote_file_list(path)
+    else:
+        return get_local_file_list(path)
+
+def make_file_path(target, file_path):
+    if path_is_remote(target):
+        remote_info = target.split(":")
+        return remote_info[0] + ":" + os.path.join(remote_info[1], file_path)
+    else:
+        return os.path.join(target, file_path)
+
+
+files = get_file_list(sys.argv[1])
+print(f"Will transfer files: {files}")
+
+for f in files:
+    from_path = make_file_path(sys.argv[1], f)
+    to_path = os.path.dirname(make_file_path(sys.argv[2], f)) + "/"
+    print(f"Will transfer {from_path} to {to_path}")
+    out = subprocess.run(["rsync", "-avsP", from_path, to_path], capture_output=True)
+    print(out.stdout.decode("utf8"))
+    print(out.stderr.decode("utf8"))
+
